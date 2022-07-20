13ac0ab0747f4c8b434ff4c00c27028858274436
reno92 <72252580+reno92@users.noreply.github.com>
2022-07-20 11:42:19 +0200

    Update f_bdd.php

--- geststages/include/function/f_bdd.php
+++ geststages/include/function/f_bdd.php
@@ -4,7 +4,7 @@ function connexionBDD()
 {
 	try
 	{
-            $bdd = new PDO('mysql:host=localhost;port=3306;dbname=azdatabase;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $bdd = new PDO('mysql:host=vhugo@azdatabase;port=3306;dbname=azdatabase;charset=utf8', 'usergs', 'mdpGS', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             return $bdd;
 	}
 	catch(Exception $e)
